113f05dd3398c1d268381d1cceff8b65129f80db
Dimifabbridf96 <dimi.fabbri@gmail.com>
2023-01-27 20:57:42 +0000

    still adding new tests to products views

--- products/tests.py
+++ products/tests.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 
 
 class ProductListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='Dimi', password='pass')
+
     def test_can_list_products(self):
         response = self.client.get('/products/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -24,7 +27,30 @@ class ProductListViewTests(APITestCase):
 
 class ProductDetailsViewTests(APITestCase):
     def setUp(self):
-         User.objects.create_user(username='Dimi', password='pass')
+        dimi = User.objects.create_user(username='Dimi', password='pass')
+        alessia = User.objects.create_user(username='Alessia', password='word')
+
+        Products.objects.create(owner=dimi, title='chair', description='ergonomic')
+        Products.objects.create(owner=alessia, title='makeup palette', description='bright and matty')
+
+    def test_can_show_detail_products_page(self):
+        response = self.client.get('/products/1/')
+        self.assertEqual(response.data['title'], 'chair')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_post_using_invalid_id(self):
+        response = self.client.get('/products/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_an_own_product(self):
+        self.client.login(username='Dimi', password='pass')
+        response = self.client.put('/products/1/', {'title': 'monitor'})
+        product = Products.objects.filter(pk=1).first()
+        self.assertEqual(product.title, 'monitor')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+
 
     
     
