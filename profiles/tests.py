5ee20b0a7597404c26ec7d9f47e9e5b25616db14
Dimifabbridf96 <dimi.fabbri@gmail.com>
2023-01-27 20:00:28 +0000

    added tests for profile views

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class ProfileListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='Dimi', password='pass')
+
+    def test_can_list_profiles(self):
+        dimi = User.objects.get(username='Dimi')
+        response = self.client.get('/profiles/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_get_profile_details(self):
+        dimi = User.objects.get(username='Dimi')
+        response = self.client.get('/profiles/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
