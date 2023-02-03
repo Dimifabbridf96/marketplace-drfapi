cbe68f31cbeda5ed9e30fc1f1b913b8a631212fe
Dimifabbridf96 <dimi.fabbri@gmail.com>
2023-02-03 21:31:12 +0000

    deployment on heroku

--- api_product/urls.py
+++ api_product/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route
+from .views import root_route, logout_route
 
 urlpatterns = [
     path("", root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('rest_auth.urls')),
     path('dj-rest-auth/registration/', include('rest_auth.registration.urls')),
     path('', include('profiles.urls')),
