20c5a31dfdc4eb75385241448d7dda301384836d
Dimifabbridf96 <dimi.fabbri@gmail.com>
2023-02-03 20:19:31 +0000

    added root route view

--- api_product/settings.py
+++ api_product/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        if 'DEV' in os.environ:
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in 'os.environ'
             else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     ]
 }
@@ -37,6 +37,10 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER': 'api_product.serializer.CurrentUserSerializer'
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
