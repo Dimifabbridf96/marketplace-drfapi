a325159efc64e2fba7bbc945d7a873e4c8af35a6
Dimifabbridf96 <dimi.fabbri@gmail.com>
2023-02-03 20:35:16 +0000

    added created_at and updated_at serilizers in comment, defined pagination

--- api_product/settings.py
+++ api_product/settings.py
@@ -26,12 +26,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in 'os.environ'
             else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    ]
+    )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 10,
+        'DATETIME_FORMAT': '%d %m %Y',
 }
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
