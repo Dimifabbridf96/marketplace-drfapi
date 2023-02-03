33ae96fdc03bdf6d43214f091409d1f3cd8288b6
Dimifabbridf96 <dimi.fabbri@gmail.com>
2023-02-03 21:54:14 +0000

    testing because heroku doesn't work as expected

--- api_product/settings.py
+++ api_product/settings.py
@@ -121,11 +121,6 @@ if 'CLIENT_ORIGIN' in os.environ:
 
     CORS_ALLOW_CREDENTIALS = True
 
-    REST_USE_JWT = True
-    JWT_AUTH_SECURE = True
-    JWT_AUTH_COOKIE = 'my-app-auth'
-    JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
-    JWT_AUTH_SAMESITE = 'None'
 
 ROOT_URLCONF = 'api_product.urls'
 
