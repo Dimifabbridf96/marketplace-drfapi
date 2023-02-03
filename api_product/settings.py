e02d4e17e324c748df669191b9260c0d2f48d922
Dimifabbridf96 <dimi.fabbri@gmail.com>
2023-02-03 19:58:17 +0000

    added DEV parameter in env.py and difference if in production or not

--- api_product/settings.py
+++ api_product/settings.py
@@ -25,6 +25,19 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        if 'DEV' in os.environ:
+            else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    ]
+}
+
+REST_USE_JWT = True
+JWT_AUTH_SECURE = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
