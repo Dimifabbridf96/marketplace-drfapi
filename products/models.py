71dc25bea514ff7c2012d0b312f9a600d344a026
Dimifabbridf96 <dimi.fabbri@gmail.com>
2023-02-22 17:42:11 +0000

    added blank and null to product

--- products/models.py
+++ products/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
-from category.models import Category
 
 
 class Products(models.Model):
+    CATEGORIES = {
+    ('Other', 'Other'),
+    ('Fashion', 'Fashion'),
+    ('Beauty', 'Beauty'),
+    ('Home & Garden', 'Home & Garden'),
+    ('Toys & Game', 'Toys & Garden'),
+    ('Sport & Outdoor', 'Sport & Outdoor'),
+    ('Pet Supply', 'Pet Supply'),
+    ('Books', 'Books'),
+    ('Electronics', 'Electronics'),
+    ('Car & Motorbike', 'Car & Motorbike')
+    }
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -12,7 +23,7 @@ class Products(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField(max_length=300, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
+    category = models.CharField(max_length=50, choices=CATEGORIES, default='Other')
     
 
     class Meta:
