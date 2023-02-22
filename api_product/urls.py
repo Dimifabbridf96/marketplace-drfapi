389196f1bdc63bbfb139a728be8a90172e0ba795
Dimifabbridf96 <dimi.fabbri@gmail.com>
2023-02-22 14:11:10 +0000

    modified requirements.txt

--- api_product/urls.py
+++ api_product/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('', include('profiles.urls')),
     path('', include('products.urls')),
     path('', include('comments.urls')),
