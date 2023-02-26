320737c66dd144ac1b3fe1e34e4b53d3337ae92b
Dimifabbridf96 <dimi.fabbri@gmail.com>
2023-02-26 20:25:13 +0000

    added serializer for review

---
+++ reviews/serializer.py
@@ -0,0 +1,31 @@
+from .models import Reviews
+from rest_framework import serializers
+from django.contrib.humanize.templatetags.humanize import naturaltime
+
+
+class ReviewSerializers(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+        
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'product', 'review', 'profile_id', 'profile_image', 'is_owner'
+        ]
+
+class CommentDetailSerializer(ReviewSerializers):
+    product = serializers.ReadOnlyField(source='products.id')
